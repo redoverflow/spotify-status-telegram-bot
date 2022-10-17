aec515c3beb63e8f2abe9fc25fbe12fe3c72f39d
redoverflow <punctuwuation@gmail.com>
2022-10-17 16:43:02 +0300

    add check for when nothing is playing

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import simplejson as json
 config = json.load(open("config.json", encoding="utf-8"))
 sleep_time = config["sleep_time"]
 prefix_suffix = [config["prefix"], config["suffix"]]
+idle_msg = config["idle"]
 
 app = Client("my_account", api_id=config["api_id"], api_hash=config["api_hash"])
 #app = Client("my_account")
@@ -26,6 +27,8 @@ def song_check_loop(app, prefix_suffix):
         stylized_song_name = prefix_suffix[0] + song_name + prefix_suffix[1]
         app.invoke(functions.account.UpdateProfile(about=stylized_song_name))
         prev_song_name = song_name
+    elif song_name == "Spotify Free":
+        app.invoke(functions.account.UpdateProfile(about=idle_msg))
     else:
         prev_song_name = song_name
     time.sleep(sleep_time)
